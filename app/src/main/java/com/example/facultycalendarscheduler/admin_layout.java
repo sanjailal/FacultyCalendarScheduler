326b13d56e0f0f18d8ae0cbf4df2559ac6252dfe
Sanjai Lal <sanjailal4@gmail.com>
2020-02-06 01:08:53 +0530

    Sign up for new faculty added

--- app/src/main/java/com/example/facultycalendarscheduler/admin_layout.java
+++ app/src/main/java/com/example/facultycalendarscheduler/admin_layout.java
@@ -2,13 +2,30 @@ package com.example.facultycalendarscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class admin_layout extends AppCompatActivity {
+public class admin_layout extends AppCompatActivity implements View.OnClickListener {
+
+    private Button addfaculty;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_layout);
+        addfaculty = findViewById(R.id.addFaculty);
+        addfaculty.setEnabled(true);
+        addfaculty.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i= view.getId();
+        if(i==R.id.addFaculty){
+            Intent Signup = new Intent(this,signUp.class);
+            startActivity(Signup);
+        }
     }
 }
