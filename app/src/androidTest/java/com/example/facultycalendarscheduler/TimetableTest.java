1e14ee5d34998038414d0bd76a80767ff8e54abe
crazyotakuu <42383388+crazyotakuu@users.noreply.github.com>
2020-03-11 12:22:57 +0530

    Merge branch 'master' of C:\Users\manasa.000\AndroidStudioProjects\FacultyCalendarSchedulerNew with conflicts.

---
+++ app/src/androidTest/java/com/example/facultycalendarscheduler/TimetableTest.java
@@ -0,0 +1,17 @@
+package com.example.facultycalendarscheduler;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class TimetableTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
---
+++ app/src/androidTest/java/com/example/facultycalendarscheduler/admin_layoutTest.java
@@ -0,0 +1,18 @@
+package com.example.facultycalendarscheduler;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class admin_layoutTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
